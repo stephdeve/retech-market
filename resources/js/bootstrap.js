4dca4cd6641bc8e4f10b31e41563b062cd48e9bb
stephdeve <stephdev6@gmail.com>
2026-01-09 20:10:01 +0100

    Fix: Hardcode Pusher credentials as fallback for Vite build

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,11 +16,11 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    key: import.meta.env.VITE_PUSHER_APP_KEY || '2b9fc757a0fee41d9b8f',
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER || 'eu',
+    wsHost: import.meta.env.VITE_PUSHER_HOST || `ws-eu.pusher.com`,
+    wsPort: import.meta.env.VITE_PUSHER_PORT || 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT || 443,
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
