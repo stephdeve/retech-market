310937c9f2dbc0ac76030dc0eee29d4c92f02aa0
stephdeve <stephdev6@gmail.com>
2026-01-13 19:58:51 +0100

    style: Replace SVGa icons with Font Awesome in product forms

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-bold text-2xl text-gray-800 dark:text-gray-200 leading-tight">
-            📢 {{ __('Nouvelle annonce') }}
+            <i class="fas fa-bullhorn mr-2"></i> {{ __('Nouvelle annonce') }}
         </h2>
     </x-slot>
 
